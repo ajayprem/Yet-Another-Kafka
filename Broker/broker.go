ba27312b5073c3d172ca892faa25a2439d28eb7d
Ajay Prem <ajay.prem@gmail.com>
2023-08-02 16:50:21 +0530

    consumer now reads everything from the begining

--- Broker/broker.go
+++ Broker/broker.go
@@ -10,6 +10,7 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -20,35 +21,35 @@ const (
 	LOGS_LOCATION = "/tmp"
 )
 
-func createTopic(topicName string, partitions int) *os.File {
-	topicDir := filepath.Join("tmp", topicName+"0")
+func createTopic(topicName string, partitions int) {
+	topicDir := filepath.Join(LOGS_LOCATION, topicName+strconv.Itoa(partitions))
 
 	if _, err := os.Stat(topicDir); os.IsNotExist(err) {
 		if err := os.Mkdir(topicDir, os.ModePerm); err != nil {
-			log.Fatal(err)
+			log.Fatalf("Unable to create topic folder: %s", err)
 		}
 		file, err := os.Create(filepath.Join(topicDir, "log.txt"))
 		if err != nil {
 			log.Fatalf("Failed creating file: %s", err)
 		}
-		return file
+		file.Close()
 	}
-	file, err := os.OpenFile(filepath.Join(topicDir, "log.txt"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Fatalf("Failed opening file: %s", err)
-	}
-	return file
 }
 
 func ProduceHandler(w http.ResponseWriter, r *http.Request) {
 	var command utils.ProduceCommand
 	json.NewDecoder(r.Body).Decode(&command)
 
-	file := createTopic(command.TopicName, 0)
+	createTopic(command.TopicName, 0)
+	topicDir := filepath.Join(LOGS_LOCATION, command.TopicName+strconv.Itoa(command.Partitions))
+	file, err := os.OpenFile(filepath.Join(topicDir, "log.txt"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("Failed opening file: %s", err)
+	}
 	defer file.Close()
 
 	datawriter := bufio.NewWriter(file)
-	_, err := datawriter.WriteString(command.Message + "\n")
+	_, err = datawriter.WriteString(command.Message + "\n")
 	if err != nil {
 		log.Fatalf("Unable to write to file: %s", err)
 	}
@@ -61,8 +62,26 @@ func ConsumeHandler(w http.ResponseWriter, r *http.Request) {
 	var command utils.ConsumeCommand
 	json.NewDecoder(r.Body).Decode(&command)
 
-	file := createTopic(command.TopicName, 0)
+	createTopic(command.TopicName, 0)
+	topicDir := filepath.Join("tmp", command.TopicName+"0")
+	file, _ := os.Open(filepath.Join(topicDir, "log.txt"))
 	defer file.Close()
+
+	fileScanner := bufio.NewScanner(file)
+	fileScanner.Split(bufio.ScanLines)
+	var messages []string
+
+	for fileScanner.Scan() {
+		messages = append(messages, fileScanner.Text())
+	}
+
+	jsonResponse, jsonError := json.Marshal(messages)
+	if jsonError != nil {
+		fmt.Println("Unable to encode JSON")
+	}
+	fmt.Println(jsonResponse)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonResponse)
 
 }
 
--- Consumer/consumer.go
+++ Consumer/consumer.go
@@ -21,25 +21,29 @@ func main() {
 	flag.IntVar(&Partition, "partition", 0, "Partition to write to")
 	flag.Parse()
 
-	for {
-		var body utils.ConsumeCommand
-		body.TopicName = TopicName
-		body.Partitions = 0
+	// for {
+	var body utils.ConsumeCommand
+	body.TopicName = TopicName
+	body.Partitions = 0
 
-		jsonBody, _ := json.Marshal(body)
-		bodyReader := bytes.NewReader(jsonBody)
+	jsonBody, _ := json.Marshal(body)
+	bodyReader := bytes.NewReader(jsonBody)
 
-		req, err := http.NewRequest(http.MethodPost, URL, bodyReader)
-		if err != nil {
-			fmt.Printf("client: could not create request: %s\n", err)
-			os.Exit(1)
-		}
-
-		res, err := http.DefaultClient.Do(req)
-		if err != nil {
-			fmt.Printf("client: error making http request: %s\n", err)
-			os.Exit(1)
-		}
-		fmt.Printf("client: status code: %d\n", res.StatusCode)
+	req, err := http.NewRequest(http.MethodPost, URL, bodyReader)
+	if err != nil {
+		fmt.Printf("client: could not create request: %s\n", err)
+		os.Exit(1)
 	}
+
+	res, err := http.DefaultClient.Do(req)
+
+	var messages []string
+	json.NewDecoder(res.Body).Decode(&messages)
+	fmt.Println(messages)
+	if err != nil {
+		fmt.Printf("client: error making http request: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("client: status code: %d\n", res.StatusCode)
+	// }
 }
